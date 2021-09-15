bffe395d974113cd6d45433772fdf1bbec729634
Serhii Nefodov <nefedovs89@gmail.com>
2021-09-15 15:10:48 +0200

    Added format arg to the parser description command

--- gendiff/src/main.py
+++ gendiff/src/main.py
@@ -5,6 +5,12 @@ def make_parser():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument(
+        '--f',
+        '--format',
+        metavar='FORMAT',
+        help='set format of output'
+    )
 
     return parser
 
