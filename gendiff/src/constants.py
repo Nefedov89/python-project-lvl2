627833a7312f1df8d1e5181b9dc9a231df9e58e4
Serhii Nefodov <nefedovs89@gmail.com>
2021-10-04 00:57:48 +0200

    Renamed constants

--- gendiff/src/constants.py
+++ gendiff/src/constants.py
@@ -1,3 +1,3 @@
-ADDED_IN_FIRST_FILE_OPERATION = '-'
-ADDED_IN_SECOND_FILE_OPERATION = '+'
-NOT_CHANGED_OPERATION = ' '
+ADDED_IN_FIRST_FILE_OPERATION = 'added_in_the_first_file'
+ADDED_IN_SECOND_FILE_OPERATION = 'added_in_the_second_file'
+NOT_CHANGED_OPERATION = 'file_was_not_changed'
